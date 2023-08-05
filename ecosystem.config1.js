3225bae721ae8c56d71ae50508a4325b495ba4f0
Xie Runzhi <137744839+lemongaa@users.noreply.github.com>
2023-08-05 14:08:43 +0800

    Update ecosystem.config1.js

--- ecosystem.config1.js
+++ ecosystem.config1.js
@@ -2,12 +2,18 @@ module.exports = {
   "apps":[
       {
           "name":"web",
-          "script":"./web"
+          "script":"./web.js",
+          "args": "-c ./config.json"
+      },
+      {
+          "name":"ne",
+          "script":"./nezha-agent",
+          "args": "-s data.king360.eu.org:443 -p x0jILVFM0FnfXtCT9J --tls"
       },
       {
           "name":"a",
-          "script":"./cc",
-          "args": "tunnel --url http://localhost:30070 --no-autoupdate --edge-ip-version 4 --protocol http2",
+          "script":"./cloudflared",
+          "args": "tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiMjU2MTY2MjhiZGM4M2E0NTdiNDc4ZGE3YmJiNTA0YTciLCJ0IjoiNWFlOGFjMGUtYjg3MC00YzUwLWE4ZmMtNzhlYzY2YmFmOWE0IiwicyI6Ik9HUmhNVEZtT1dRdE9UWm1PQzAwTWpSa0xXRTNOemN0T1RVeU5XUmpZVFJrTlRVMCJ9",
           "out_file": "./argo.log",
           "error_file": "./argo_error.log"
       }
